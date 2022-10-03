f2923a16fcdafcd44a279ffb63a012f2ac516d3e
vishalfulcrum <vpatil@redstage.com>
2022-10-03 16:32:37 +0530

    Compatible with Atomic

--- Gateway/Http/Client/CancelSale.php
+++ Gateway/Http/Client/CancelSale.php
@@ -58,8 +58,14 @@ class CancelSale implements ClientInterface
     public function placeRequest(TransferInterface $transferObject)
     {
         $request = $transferObject->getBody();
-        // Set cancel url
-        $url = $this->apexxBaseHelper->getApiEndpoint().$request['transactionId'].'/cancel';
+        $apiType = $this->apexxBaseHelper->getApiType();
+        if ($apiType == 'Atomic') {
+            $url = $this->apexxBaseHelper->getApiEndpoint().'cancel/payment/'.$request['transactionId'];
+        } else {
+            // Set cancel url
+            $url = $this->apexxBaseHelper->getApiEndpoint().$request['transactionId'].'/cancel';
+        }
+
         //Set parameters for curl
         unset($request['transactionId']);
         $resultCode = json_encode($request);
